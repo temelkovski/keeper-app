99174c7d4018dbc44aa5871468d090a559046f58
temelkovski <temelkovski.dimitar@gmail.com>
2022-03-01 23:14:35 +0100

    Configure app for deployment to GitHub Pages

---
+++ build/precache-manifest.02ebf6575b15d6f9a3005af7e3eb87c8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56327342c3677552fe5ad0e525845698",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "aac965a2db5372c59910",
+    "url": "/keeper-app/static/js/2.246fee9e.chunk.js"
+  },
+  {
+    "revision": "b03c3b7b39b9388f2159",
+    "url": "/keeper-app/static/js/main.ce4a743e.chunk.js"
+  },
+  {
+    "revision": "78797e502974e5d55e3c",
+    "url": "/keeper-app/static/js/runtime-main.e57cdbd2.js"
+  }
+]);
